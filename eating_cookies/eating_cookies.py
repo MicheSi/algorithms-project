3fa297e66a4f423e5922aef36598445de2c24d86
mlazzell17@gmail.com <MicheSi>
2020-05-20 15:45:58 -0700

    working on eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,16 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
-
-    pass
+    # Way 1: eat 1 + 1 + 1 cookies
+    # Way 2: eat 1 + 2 cookies 
+    # Way 3: eat 3 cookies
+    # Way 4: eat 2 + 1 cookie 
+    # return should be 4
+    if n == 0 or n == 1:
+        return 1
+    if n <= 2:
+        return n
+    return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
